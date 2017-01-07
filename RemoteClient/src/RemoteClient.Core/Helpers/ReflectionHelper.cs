6897eefe352fc98e70405203bdb568d59a2de5c5
Pzixel <pzixel@gmail.com>
2017-01-07 04:20:00 +0300

    further porting

--- RemoteClient/src/RemoteClient.Core/Helpers/ReflectionHelper.cs
+++ RemoteClient/src/RemoteClient.Core/Helpers/ReflectionHelper.cs
@@ -9,8 +9,8 @@ namespace WcfRestClient.Core.Helpers
 {
     internal static class ReflectionHelper
     {
-        public static MethodInfo DisposeMethod { get; } = typeof (IDisposable).GetMethod("Dispose");
-        public static Type GetPropertyInterfaceImplementation<T>() where T : class => InterfaceImplementator<T>.Value;
+        public static MethodInfo DisposeMethod { get; } = typeof (IDisposable).GetTypeInfo().GetDeclaredMethod("Dispose");
+        public static TypeInfo GetPropertyInterfaceImplementation<T>() where T : class => InterfaceImplementator<T>.Value;
 
         public static ModuleBuilder Builder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("ServiceClientFactoryAssembly"), AssemblyBuilderAccess.Run)
                                                              .DefineDynamicModule("MainModule");
@@ -31,11 +31,11 @@ namespace WcfRestClient.Core.Helpers
                 throw new ArgumentException("Method doesn't have WebGet or WebInvoke attribute");
             }
 
-            var attributeType = uriAttribute.GetType();
-            string method = attributeType.FullName == WebGetFullName ? "GET" : (string) attributeType.GetProperty("Method").GetValue(uriAttribute);
-            string uriTemplate = (string) attributeType.GetProperty("UriTemplate").GetValue(uriAttribute);
-            var requestFormat = (OperationWebMessageFormat) attributeType.GetProperty("RequestFormat").GetValue(uriAttribute);
-            var responseFormat = (OperationWebMessageFormat) attributeType.GetProperty("ResponseFormat").GetValue(uriAttribute);
+            var attributeType = uriAttribute.GetType().GetTypeInfo();
+            string method = attributeType.FullName == WebGetFullName ? "GET" : (string) attributeType.GetDeclaredProperty("Method").GetValue(uriAttribute);
+            string uriTemplate = (string) attributeType.GetDeclaredProperty("UriTemplate").GetValue(uriAttribute);
+            var requestFormat = (OperationWebMessageFormat) attributeType.GetDeclaredProperty("RequestFormat").GetValue(uriAttribute);
+            var responseFormat = (OperationWebMessageFormat) attributeType.GetDeclaredProperty("ResponseFormat").GetValue(uriAttribute);
 
             return new WcfOperationDescriptor(uriTemplate, method, requestFormat, responseFormat);
         }
@@ -50,20 +50,21 @@ namespace WcfRestClient.Core.Helpers
         private static class InterfaceImplementator<T> where T: class 
         {
             [SuppressMessage("ReSharper", "StaticMemberInGenericType")]
-            public static Type Value { get; }
+            public static TypeInfo Value { get; }
             static InterfaceImplementator()
             {
                 var interfaceType = typeof(T);
-                if (!interfaceType.IsInterface)
+                var typeInfo = interfaceType.GetTypeInfo();
+                if (!typeInfo.IsInterface)
                 {
-                    throw new ArgumentException($"{interfaceType.FullName} should be an interface!");
+                    throw new ArgumentException($"{typeInfo.FullName} should be an interface!");
                 }
-                var interfaceProps = interfaceType.GetProperties();
-                if (interfaceType.GetMethods().Except(interfaceProps.Select(x => x.GetMethod).Concat(interfaceProps.Select(x => x.SetMethod))).Any())
+                var interfaceProps = typeInfo.DeclaredProperties.ToList();
+                if (typeInfo.DeclaredMethods.Except(interfaceProps.Select(x => x.GetMethod).Concat(interfaceProps.Select(x => x.SetMethod))).Any())
                 {
-                    throw new ArgumentException($"{interfaceType.FullName} must have properties only!");
+                    throw new ArgumentException($"{typeInfo.FullName} must have properties only!");
                 }
-                var tb = Builder.DefineType($"<{interfaceType.Name}>__Implementation", TypeAttributes.Class | TypeAttributes.Sealed);
+                var tb = Builder.DefineType($"<{typeInfo.Name}>__Implementation", TypeAttributes.Class | TypeAttributes.Sealed);
                 foreach (var interfaceProp in interfaceProps)
                 {
                     var prop = tb.EmitAutoProperty(interfaceProp.Name, interfaceProp.PropertyType);
@@ -77,7 +78,7 @@ namespace WcfRestClient.Core.Helpers
                     }
                 }
                 tb.AddInterfaceImplementation(interfaceType);
-                Value = tb.CreateType();
+                Value = tb.CreateTypeInfo();
             }
         }
     }
